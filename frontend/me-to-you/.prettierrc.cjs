640f9afda4d0f5e06bb614445bf977858cd5357f
백하람 <developer.venice@gmail.com>
2024-10-23 16:28:41 +0900

    init project

---
+++ frontend/me-to-you/.prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    semi: true,
+    singleQuote: false,
+    trailingComma: "es5",
+    printWidth: 100,
+    tabWidth: 2,
+    useTabs: false,
+    arrowParens: "avoid",
+    bracketSpacing: true,
+    bracketSameLine: false,
+    quoteProps: "consistent",
+    endOfLine: "auto",
+};
