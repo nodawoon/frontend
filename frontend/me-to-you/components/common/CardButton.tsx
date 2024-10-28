b2a25a37dc8353952445917224095ed945c8880a
서근범 <seo6893@naver.com>
2024-10-28 13:57:27 +0900

    feat: CardButton 추가

---
+++ frontend/me-to-you/components/common/CardButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { ReactNode } from "react";
+
+interface CardButtonProps {
+  type?: "submit" | undefined;
+  className?: string;
+  onClick?: () => void;
+  title: ReactNode;
+  text: ReactNode;
+}
+
+function CardButton({ type, className, onClick, title, text }: CardButtonProps) {
+  const combinedClassName =
+    "flex flex-col justify-center h-20 w-96 px-3.5 text-base text-black bg-white border border-gray font-bold break-words rounded-lg hover:bg-soft-gray";
+
+  return (
+    <button
+      type={type ? type : "button"}
+      className={`${combinedClassName} ${className}`}
+      onClick={onClick}
+    >
+      <div className="flex w-full justify-between py-0.5 text-lg">
+        <div className="w-auto">{title}</div>
+        <span className="material-symbols-rounded text-icon">arrow_forward</span>
+      </div>
+      <div className="py-0.5 text-sm font-light">{text}</div>
+    </button>
+  );
+}
+
+export default CardButton;
