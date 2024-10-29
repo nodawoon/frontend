6d6a0a501f0f6eded2d4ea5c00a4947339331507
서근범 <seo6893@naver.com>
2024-10-29 12:10:20 +0900

    feat: home-page 개발

--- frontend/me-to-you/app/page.tsx
+++ frontend/me-to-you/app/page.tsx
@@ -1,5 +1,42 @@
+"use client";
+
+import CardButton from "@/components/common/CardButton";
+
 export default function Home() {
-  return <>
-    <div className="font-bold underline">메인페이지</div>
-  </>;
+  return (
+    <>
+      <div className="flex flex-col mx-auto h-screen justify-center">
+        <p className="text-center my-6 text-[26px] font-bold">
+          <span className="text-primary">남</span>이 보는 <span className="text-primary">내</span>{" "}
+          모습을 알아보세요.
+        </p>
+        <div className="flex flex-col mx-auto w-[80%]">
+          <CardButton
+            page={1}
+            className="my-1.5 min-w-full"
+            title="설문 생성"
+            text="설문을 생성해서 주변 사람들에게 공유해보세요."
+          ></CardButton>
+          <CardButton
+            page={2}
+            className="my-1.5 min-w-full"
+            title="결과 확인"
+            text="주변 사람들은 나를 어떻게 생각할까요?"
+          ></CardButton>
+          <CardButton
+            page={3}
+            className="my-1.5 min-w-full"
+            title="챗봇 생성"
+            text="챗봇에게 나에 대한 정보를 알려주세요."
+          ></CardButton>
+          <CardButton
+            page={4}
+            className="my-1.5 min-w-full"
+            title="채팅 보기"
+            text="어떤 사람이 나의 챗봇과 대화했는지 확인하세요."
+          ></CardButton>
+        </div>
+      </div>
+    </>
+  );
 }
--- frontend/me-to-you/components/common/CardButton.tsx
+++ frontend/me-to-you/components/common/CardButton.tsx
@@ -1,35 +1,58 @@
 "use client";
 
+import Link from "next/link";
+
 interface CardButtonProps {
-  type?: "submit" | undefined;
   className?: string;
-  onClick?: () => void;
+  onClick?: void;
+  page: number;
   title: string;
   text: string;
 }
 
 const CardButton: React.FC<CardButtonProps> = ({
-  type,
   className,
   onClick,
+  page,
   title,
   text,
 }: CardButtonProps) => {
   const combinedClassName =
-    "flex flex-col justify-center h-20 w-96 px-3.5 text-base text-black bg-white border border-gray font-bold break-words rounded-lg hover:bg-soft-gray";
+    "flex flex-col justify-center h-20 px-3.5 text-base text-black bg-white border border-gray font-bold break-words rounded-lg hover:bg-soft-gray";
+  let url: string = "/";
+
+  switch (page) {
+    case 1:
+      url += "설문";
+      break;
+    case 2:
+      url += "결과";
+      break;
+    case 3:
+      url += "챗봇";
+      break;
+    case 4:
+      url += "채팅";
+      break;
+  }
+
+  const errorMessage = () => {
+    if (page !== 3) return;
+    alert("아직 준비중입니다.");
+  };
 
   return (
-    <button
-      type={type ? type : "button"}
+    <Link
+      href={page !== 3 ? url : ""}
       className={`${combinedClassName} ${className}`}
-      onClick={onClick}
+      onClick={errorMessage}
     >
       <div className="flex w-full justify-between py-0.5 text-lg">
         <div className="w-auto">{title}</div>
         <span className="material-symbols-rounded text-icon">arrow_forward</span>
       </div>
       <div className="py-0.5 text-sm font-light">{text}</div>
-    </button>
+    </Link>
   );
 };
 
