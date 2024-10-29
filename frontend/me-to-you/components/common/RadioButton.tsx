e5a8b7d8304b9dd90449fb2b15a4415b7e1cff2d
백하람 <developer.venice@gmail.com>
2024-10-29 17:34:09 +0900

    feat: 회원가입 페이지 개발

--- frontend/me-to-you/components/common/RadioButton.tsx
+++ frontend/me-to-you/components/common/RadioButton.tsx
@@ -1,47 +1,81 @@
+"use client";
+
 import React from "react";
 
 interface RadioProps {
   type?: string;
+  selectedValue: string;
+  onChange: (value: string) => void;
 }
 
-//베이스는 성별, "question_type" 입력시에는 문제 유형 선택
+function RadioButton({ type = "gender", selectedValue, onChange }: RadioProps) {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
 
-function RadioButton({ type = "gender" }: RadioProps) {
-  return (
-    <div>
-      {type === "gender" ? (
-        // 성별 선택 라디오 버튼
-        <div className="h-10 flex space-x-6 justify-between items-center">
-          <div className="flex items-center space-x-2">
-            <input type="radio" value="남자" name="성별" />
-            <label className="text-sm font-bold">남자</label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <input type="radio" value="여자" name="성별" />
-            <label className="text-sm font-bold">여자</label>
-          </div>
+  if (type === "gender") {
+    return (
+      <div className="h-10 flex space-x-6 justify-between items-center">
+        <div className="flex items-center space-x-2">
+          <input
+            type="radio"
+            value="MAN"
+            name="gender"
+            checked={selectedValue === "MAN"}
+            onChange={handleChange}
+          />
+          <label className="text-sm font-bold">남자</label>
         </div>
-      ) : type === "question_type" ? (
-        // 문제 유형 선택 라디오 버튼
-        <div className="flex space-x-6 justify-between items-center">
-          <div className="flex items-center space-x-2">
-            <input type="radio" value="multiple_choice" name="question_type" />
-            <label className="text-sm font-bold">객관식</label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <input type="radio" value="short_answer" name="question_type" />
-            <label className="text-sm font-bold">단답형</label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <input type="radio" value="long_answer" name="question_type" />
-            <label className="text-sm font-bold">서술형</label>
-          </div>
+        <div className="flex items-center space-x-2">
+          <input
+            type="radio"
+            value="WOMAN"
+            name="gender"
+            checked={selectedValue === "WOMAN"}
+            onChange={handleChange}
+          />
+          <label className="text-sm font-bold">여자</label>
         </div>
-      ) : (
-        <p>알 수 없는 양식</p>
-      )}
-    </div>
-  );
+      </div>
+    );
+  } else if (type === "question_type") {
+    return (
+      <div className="flex space-x-6 justify-between items-center">
+        <div className="flex items-center space-x-2">
+          <input
+            type="radio"
+            value="multiple_choice"
+            name="question_type"
+            checked={selectedValue === "multiple_choice"}
+            onChange={handleChange}
+          />
+          <label className="text-sm font-bold">객관식</label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <input
+            type="radio"
+            value="short_answer"
+            name="question_type"
+            checked={selectedValue === "short_answer"}
+            onChange={handleChange}
+          />
+          <label className="text-sm font-bold">단답형</label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <input
+            type="radio"
+            value="long_answer"
+            name="question_type"
+            checked={selectedValue === "long_answer"}
+            onChange={handleChange}
+          />
+          <label className="text-sm font-bold">서술형</label>
+        </div>
+      </div>
+    );
+  } else {
+    return <p>알 수 없는 양식</p>;
+  }
 }
 
 export default RadioButton;
--- frontend/me-to-you/components/common/TextInput.tsx
+++ frontend/me-to-you/components/common/TextInput.tsx
@@ -1,37 +1,45 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { ChangeEventHandler, useState } from "react";
 
 interface textInputProps {
   placeholder?: string;
-  width: number;
+  value?: string;
+  validationMessage?: string;
+  handleChangeInput: ChangeEventHandler<HTMLInputElement>;
 }
 
-function TextInput({ placeholder = "이메일", width }: textInputProps) {
+function TextInput({
+  placeholder = "이메일",
+  value = "",
+  validationMessage,
+  handleChangeInput,
+}: textInputProps) {
   const [isFocused, setIsFocused] = useState(false);
-  const [inputValue, setInputValue] = useState("");
 
   return (
-    <div className="relative" style={{ width: `${width}%` }}>
+    <div className="relative w-full">
       <input
         type="text"
-        value={inputValue}
-        onChange={e => setInputValue(e.target.value)}
+        value={value}
+        onChange={handleChangeInput}
         onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(inputValue !== "")}
-        style={{ width: `${width}%` }}
-        className={`h-10 border border-gray p-2 rounded-md outline-none focus:ring-1 focus:ring-primary transition-all z-10`}
+        onBlur={() => setIsFocused(value !== "")}
+        className={`h-10 w-full border border-gray p-2 rounded-md outline-none focus:ring-1 focus:ring-primary transition-all z-10`}
       />
       <label
         className={`absolute left-2 px-1 text-sm mt-1 text-gray bg-white transition-all duration-300 ease-in-out pointer-events-none
           ${
-            isFocused || inputValue
+            isFocused || value
               ? "-top-2.5 left-2 text-xs text-primary z-20" // 위로 이동
               : "top-2 left-2 text-base text-gray-500"
           }`} // 기본 위치
       >
         {placeholder}
       </label>
+      <span className="text-xs text-gray absolute -bottom-4 left-2">
+        {isFocused && validationMessage}
+      </span>
     </div>
   );
 }
