915203c7b4a9af2bf8fed63d373e529f1787a155
백하람 <developer.venice@gmail.com>
2024-11-07 08:50:20 +0900

    !HOTFIX: /login으로 시작하는 경로도 허용

--- frontend/me-to-you/app/AuthProvider.tsx
+++ frontend/me-to-you/app/AuthProvider.tsx
@@ -12,7 +12,7 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
 
   useEffect(() => {
     const isLogin = sessionStorage.getItem("isLogin");
-    const isPublicPath = pathname.startsWith(ROUTES.LOGIN);
+    const isPublicPath = pathname.startsWith(ROUTES.LOGIN) || pathname.startsWith("/login");
     const isSurvey = pathname.startsWith("/survey/invitation");
 
     if (pathname === "/signup" && isLogin) {
