5651c0d24b2a00b216a62d733e6727d6c655d7e0
백하람 <developer.venice@gmail.com>
2024-11-08 12:27:43 +0900

    feat: ContextAPI로 로그인 정보 전역으로 전송

--- frontend/me-to-you/app/layout.tsx
+++ frontend/me-to-you/app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import Header from "@/components/layout/Header";
 import Image from "next/image";
 import ReduxProvider from "./ReduxProvider";
+import { AuthProvider } from "@/context/AuthContext";
+import { cookies } from "next/headers";
 
 export const metadata: { description: string; title: string } = {
   title: "너에게 난",
@@ -13,37 +15,50 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = cookies();
+  const token = cookieStore.get("ME_TO_YOU_TOKEN")?.value;
+  let isLoggedIn = false;
+
+  if (token) {
+    try {
+      isLoggedIn = true;
+    } catch (error) {
+      isLoggedIn = false;
+    }
+  }
   return (
     <html lang="ko" className="h-[100%]">
       <body className="h-[100%] desktop:flex overflow-y-hidden">
         <ReduxProvider>
-          <section className="desktop:w-full max-w-[460px] w-full mx-auto h-[100%] overflow-y-scroll scrollbar-none">
-            <Header />
-            <section>{children}</section>
-          </section>
+          <AuthProvider initialIsLoggedIn={isLoggedIn}>
+            <section className="desktop:w-full max-w-[460px] w-full mx-auto h-[100%] overflow-y-scroll scrollbar-none">
+              <Header />
+              <section>{children}</section>
+            </section>
 
-          <section className="desktop:intro-section-desktop hidden">
-            <div className="h-1/2"></div>
-            <Image
-              src="/logo.svg"
-              alt="'너에게 난' 로고"
-              width="350"
-              height="80"
-              priority={true}
-              style={{ width: "350px", height: "80px" }}
-            />
-            <div>
-              <p className="mt-5">
-                ‘너에게 난 어떤 사람일까?’ 라는 궁금증에 대한 해답을 담았습니다.
+            <section className="desktop:intro-section-desktop hidden">
+              <div className="h-1/2"></div>
+              <Image
+                src="/logo.svg"
+                alt="'너에게 난' 로고"
+                width="350"
+                height="80"
+                priority={true}
+                style={{ width: "350px", height: "80px" }}
+              />
+              <div>
+                <p className="mt-5">
+                  ‘너에게 난 어떤 사람일까?’ 라는 궁금증에 대한 해답을 담았습니다.
+                </p>
+                <p className="mb-40">
+                  메인 색상인 파란색은 신뢰를 나타내며 고양이 캐릭터는 무의식을 의미합니다.
+                </p>
+              </div>
+              <p className="text-sm font-light text-dark-gray mt-10">
+                Copyright 2024. SSAFY. All rights reserved.
               </p>
-              <p className="mb-40">
-                메인 색상인 파란색은 신뢰를 나타내며 고양이 캐릭터는 무의식을 의미합니다.
-              </p>
-            </div>
-            <p className="text-sm font-light text-dark-gray mt-10">
-              Copyright 2024. SSAFY. All rights reserved.
-            </p>
-          </section>
+            </section>
+          </AuthProvider>
         </ReduxProvider>
       </body>
     </html>
