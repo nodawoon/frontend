94fed6b66a373a6f3c2f34d5ebf0f83e9c4099a9
KBG1 <bumdoly200@naver.com>
2024-11-14 13:28:24 +0900

    setting: sc 파일 변수로도 가져올 수 있도록 수정

--- frontend/me-to-you/next.config.mjs
+++ frontend/me-to-you/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "me-to-you.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
